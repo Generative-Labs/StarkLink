72d44bc1299cdf6c097258fc76d8fc15d7610420
Iris <iris.devillars@gmail.com>
2023-02-02 16:48:19 +0200

    update follow btn

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -3,6 +3,7 @@ import { AppTypeEnum, LoginModal, Button } from '@web3mq/react-components';
 import useLogin from '../hooks/useLogin';
 import '@web3mq/react-components/dist/css/index.css';
 import { AddContactIcon } from './icons/AddContactIcon';
+import { UserAccountType } from '@web3mq/react-components/dist/components/LoginModal/hooks/useLogin';
 
 export type FollowButtonProps = {
   username?: string;
@@ -13,40 +14,47 @@ export type FollowButtonProps = {
 const FollowButton: FunctionComponent<FollowButtonProps> = ({}: FollowButtonProps) => {
   const [isLoggedId, setIsLoggedIn] = useState(false);
   const [isRegistering, setRegistering] = useState(false);
-  const [account, setAccount] = useState<any>(null);
-  // @ts-ignore
+  const [account, setAccount] = useState<UserAccountType>();
   const [appType, setAppType] = useState(window.innerWidth <= 600 ? AppTypeEnum['h5'] : AppTypeEnum['pc']);
   const { keys, init, getAccount, handleLoginEvent } = useLogin();
 
+  const handleAppType = () => {
+    setAppType(window.innerWidth <= 600 ? AppTypeEnum['h5'] : AppTypeEnum['pc']);
+  };
+
   useEffect(() => {
+    init();
     // set component theme
     document.getElementsByTagName('body')[0].setAttribute('data-theme', 'light');
+    window.addEventListener('resize', handleAppType);
+    () => {
+      window.removeEventListener('resize', handleAppType);
+    };
   }, []);
-
-  if (!keys) {
-    let mainKeys: any = null;
-    const mainPrivateKey = localStorage.getItem(`MAIN_PRIVATE_KEY`);
-    const mainPublicKey = localStorage.getItem(`MAIN_PUBLIC_KEY`);
-    const address = localStorage.getItem('WALLET_ADDRESS');
-    if (mainPublicKey && mainPrivateKey && address) {
-      mainKeys = {
-        publicKey: mainPublicKey,
-        privateKey: mainPrivateKey,
-        walletAddress: address,
-      };
-    }
-    console.log('mainKeys', mainKeys);
-  }
 
   const followUser = () => {
     if (!isLoggedId) {
       init().then(() => {
         getAccount().then((data) => {
-          setAccount(data);
+          console.log('data', data);
+          setAccount({ ...data, walletType: 'starknet' });
           if (data.userExist) {
             setIsLoggedIn(true);
           } else {
-            setRegistering(true);
+            if (!keys) {
+              let mainKeys: any = null;
+              const mainPrivateKey = localStorage.getItem(`MAIN_PRIVATE_KEY`);
+              const mainPublicKey = localStorage.getItem(`MAIN_PUBLIC_KEY`);
+              const address = localStorage.getItem('WALLET_ADDRESS');
+              if (mainPublicKey && mainPrivateKey && address) {
+                mainKeys = {
+                  publicKey: mainPublicKey,
+                  privateKey: mainPrivateKey,
+                  walletAddress: address,
+                };
+              }
+              setRegistering(true);
+            }
           }
         });
       });
